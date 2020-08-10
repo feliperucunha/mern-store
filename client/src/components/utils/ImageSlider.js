3df752a23f0348e15f8f204fdea2de5f180d3a0b
Felipe Cunha <feliperubenmv@gmail.com>
2020-08-10 09:44:03 -0300

    Update ImageSlider.js

--- client/src/components/utils/ImageSlider.js
+++ client/src/components/utils/ImageSlider.js
@@ -9,7 +9,7 @@ function ImageSlider(props) {
                 {props.images.map((image, index) => (
                     <div key={index}>
                         <img style={{ width: '100%', maxHeight: '150px' }}
-                            src={`http://localhost:5000/${image}`} alt="productImage" />
+                            src={`https://mern-store-burguer.herokuapp.com/${image}`} alt="productImage" />
                     </div>
                 ))}
             </Carousel>
